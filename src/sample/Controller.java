26a862e7ac4c9a73ad458afce7370ed75c2944ba
Andreas Albihn <albihn.andreas@gmail.com>
2018-10-02 14:16:39 +0200

    onAction button added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+
 public class Controller {
+
+    public void buttonAction(ActionEvent actionEvent) {
+        ((Button)actionEvent.getSource()).setText("Clicked");
+    }
 }
